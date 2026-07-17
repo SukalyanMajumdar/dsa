61bcc1b61454160bccb2b8d86ed4aa2b44104a35
Sukalyan Majumdar <sukalyanindian@gmail.com>
2026-07-17 12:20:09 +0530

    day 3

--- dsa/singly_linked_list.py
+++ dsa/singly_linked_list.py
@@ -1,51 +1,77 @@
 class Node():
-    def __init__(self, value, next=None):
+    def __init__(self,value,next_elm=None):
         self.value=value
-        self.next=next
+        self.next_elm=next_elm
 
-class SinglyLinkedList():
-    def __init__(self, value=None):
-        self.head=Node(None,None)
+class Singly_Linked_List():
+    def __init__(self,value=None):
+        self.exists=Node(None,None)
         if value is None:
-            self.tail=self.head
-            self.length=0            
+            self.head=self.tail=None
+            self.length=0
         else:
-            self.tail=Node(value,None)
-            self.head.next=self.tail
+            self.head=self.tail=Node(value)
             self.length=1
-                    
+
     def append(self, value):
-        self.tail.next=Node(value)
-        self.tail=self.tail.next
+        if self.length==0:
+            self.tail=self.head=Node(value)
+        else:
+            self.tail.next_elm=Node(value)
+            self.tail=self.tail.next_elm
         self.length+=1
-        
+
+    def printAll(self):
+        pointer=self.head
+        while pointer:
+            print(pointer.value)
+            pointer=pointer.next_elm
+
     def pop(self):
-        if self.length>0:
-            iteration=0
+        if self.length==0:
+            return
+        if self.length==1:
+            self.tail=self.head=None
+            self.length=0
+        else:
             pointer=self.head
-            while iteration<self.length-1:
-                iteration+=1
-                pointer=pointer.next
-            pointer.next=None
+            while pointer.next_elm.next_elm:
+                pointer=pointer.next_elm
+            pointer.next_elm=None
             self.tail=pointer
             self.length-=1
-            
-    def append_left(self, value):
-        if self.length>0:
-            temp=self.head.next
-            self.head.next=Node(value,temp)
+
+    def append_left(self,value):
+        if self.length==0:
+            self.append(value)
         else:
-            self.tail=Node(value)
-            self.head.next=self.tail
-        self.length+=1
-        
-    def pop_left(self):
-        if self.length==1:
-            self.head.next=None
-            self.tail=self.head
-            self.length-=1
-        elif self.length>1:
-            self.head.next=self.head.next.next
-            self.length-=1
-                                                
-linkedlist1=SinglyLinkedList()
+            temp_right_side=self.head
+            self.head=Node(value,temp_right_side)
+            self.length+=1
+
+sll1=Singly_Linked_List(2)
+
+sll1.append(5)
+
+sll1.append(7)
+
+sll1.append(10)
+
+sll1.printAll()
+print("****")
+print("length=",sll1.length)
+print("****")
+
+sll1.pop()
+
+sll1.printAll()
+print("****")
+print("length=",sll1.length)
+print("****")
+
+sll1.append_left(-200)
+
+sll1.printAll()
+print("****")
+print("length=",sll1.length)
+print("****")
